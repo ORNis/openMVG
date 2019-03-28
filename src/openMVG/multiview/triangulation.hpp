cd8968b9f8e4d9d994b0e3cd34d75c31bf605dc8
Romain Janvier <romain.janvier@lepatriscope.com>
2019-03-28 21:41:26 +0100

    [Multiview] Add a 2 view optimal L1 triangulation

--- src/openMVG/multiview/triangulation.hpp
+++ src/openMVG/multiview/triangulation.hpp
@@ -16,7 +16,7 @@ namespace openMVG
 
 /**
 * @brief Linear DLT triangulation
-* @brief P1 First camera projection matrix
+* @param P1 First camera projection matrix
 * @param P2 Second camera projection matrix
 * @param x1 bearing vector of the landmark observation in the first camera
 * @param x2 bearing vector of the landmark observation in the second camera
@@ -35,7 +35,7 @@ void TriangulateDLT
 
 /**
 * @brief Linear DLT triangulation
-* @brief P1 First camera projection matrix
+* @param P1 First camera projection matrix
 * @param P2 Second camera projection matrix
 * @param x1 bearing vector of the landmark observation in the first camera
 * @param x2 bearing vector of the landmark observation in the second camera
@@ -48,6 +48,29 @@ void TriangulateDLT
   const Vec3 &x1,
   const Mat34 &P2,
   const Vec3 &x2,
+  Vec3 *X_euclidean
+);
+
+/**
+* @brief Optimal L1 Angular triangulation
+* @brief Minimize the L1 norm of angular errors
+* @param R0 First Camera rotation matrix
+* @param t0 First Camera translation vector
+* @param x0 bearing vector of the landmark observation in the first camera
+* @param R1 Second Camera rotation matrix
+* @param t1 Second Camera translation vector
+* @param x1 bearing vector of the landmark observation in the second camera
+* @param[out] X_euclidean Euclidean triangulated point
+* @ref S.H. Lee, J. Civera - Closed-Form Optimal Triangulation Based on Angular Errors - https://arxiv.org/pdf/1903.09115.pdf
+*/
+void TriangulateL1Angular
+(
+  const Mat3 &R0,
+  const Vec3 &t0,
+  const Vec3 &x0,
+  const Mat3 &R1,
+  const Vec3 &t1,
+  const Vec3 &x1,
   Vec3 *X_euclidean
 );
 
